6d9d0b4dbce6547879dd7fd77e12945004d5118b
Tankred Hase <mail@tankredhase.de>
2019-05-10 14:47:16 -0700

    Wire up src files in App.js and add rn-cli.config.js

--- mobile-bare/App.js
+++ mobile-bare/App.js
@@ -1,56 +1,13 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import * as Random from 'expo-random';
+import 'node-libs-react-native/globals';
+import { Platform } from 'react-native';
 
-// required to setup expo-font
-StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { randomBytes: null };
-  }
-
-  async componentDidMount() {
-    const bytes = await Random.getRandomBytesAsync(16);
-    this.setState({
-      randomBytes: bytes.toString(),
-    });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.instructions}>Some randomness: {this.state.randomBytes}</Text>
-      </View>
-    );
-  }
+// Polyfill ECMAScript Internationalization API on Android
+// See: https://github.com/facebook/react-native/issues/19410
+if (Platform.OS === 'android') {
+  require('intl');
+  require('intl/locale-data/jsonp/en-US');
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import App from '../src/view/main-mobile';
+
+module.exports = App;
